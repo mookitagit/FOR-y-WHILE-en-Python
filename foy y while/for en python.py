a7ec0c189a67c436b995e7e485b67b3a9639cc5f
Jesus Ibarra <ayudamedezmayo@gmail.com>
2025-04-07 04:52:51 -0600

    Add files via upload

---
+++ foy y while/for en python.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Este bucle for imprimirá los números del 1 al 5
+
+# range(1, 6) genera los números 1, 2, 3, 4, 5 (el 6 no se incluye)
+for i in range(1, 6):
+    print(f"Número: {i}")  # Imprime el número actual
---
+++ foy y while/while en python.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  7 04:48:34 2025
+
+@author: ayuda
+"""
+
+# Inicializamos la variable contador en 1
+contador = 1
+
+# Mientras contador sea menor o igual a 7, se ejecuta el bucle
+while contador <= 7:
+    print(f"Número: {contador}")  # Imprime el valor actual de contador
+    contador += 1  # Incrementa contador en 1 en cada vuelta
